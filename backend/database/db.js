6320e69bb22e3c6922097f7e36cfef7c8a6c6a6d
cananbuyukdagg <canan@dabnet.com.tr>
2023-06-28 19:02:15 +0300

    .

---
+++ backend/database/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const uri = "mongodb+srv://cananbuyukdag:2kSd5gtS78KTB6KA@ecommercedb.kfvzkll.mongodb.net/?retryWrites=true&w=majority";
+
+const connection = () =>{
+    mongoose.connect(uri,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(()=> console.log("MongoDb bağlantısı başarılı"))
+    .catch((err)=>console.log("Bağlantısı Hatası! Hata: " + err.message));
+}
+
+module.exports = connection;
